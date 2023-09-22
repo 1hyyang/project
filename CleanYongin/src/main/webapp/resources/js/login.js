5824197be4485df6a9a4bcc5a4c6fe490af23e49
hyyang <128003856+1hyyang@users.noreply.github.com>
2023-09-22 10:19:27 +0900

    Update login.js

--- CleanYongin/src/main/webapp/resources/js/login.js
+++ CleanYongin/src/main/webapp/resources/js/login.js
@@ -18,14 +18,14 @@ function fetchPost(url, obj, callback){
 		    headers: {"Content-Type": "application/json"},
 		    // JS object를 JSON 타입의 문자열로 변환
 		    body: JSON.stringify(obj)})
-	    // 컨트롤러로부터 JSON 타입의 객체가 반환
-	    // 객체를 변수명 response에 받아 와서 json() 메소드를 호출
-	    // json() : JSON 형식의 문자열을 Promise 객체로 반환
-	    // Promise 객체는 then() 메소드를 사용하여 
-	    // 비동기 작업의 성공 또는 실패와 관련된 결과를 나타내는 대리자 역할을 수행
-	    .then(response => response.json())
-	    // 반환 받은 객체를 매개 변수로 받는 콜백 함수를 호출
-	    .then(map => callback(map));
+	// 컨트롤러로부터 JSON 타입의 객체가 반환
+	// 객체를 변수명 response에 받아 와서 json() 메소드를 호출
+	// json() : JSON 형식의 문자열을 Promise 객체로 반환
+	// Promise 객체는 then() 메소드를 사용하여 
+	// 비동기 작업의 성공 또는 실패와 관련된 결과를 나타내는 대리자 역할을 수행
+	.then(response => response.json())
+	// 반환 받은 객체를 매개 변수로 받는 콜백 함수를 호출
+	.then(map => callback(map));
 }
 
 function loginResult(result){
